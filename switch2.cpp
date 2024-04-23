dee0e187e50d3814511c485f9b913892041dd204
KENTEA <131833594+KenTea0@users.noreply.github.com>
2024-04-23 14:46:41 +0000

    これが正しい書きかた

---
+++ switch2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 2;
+    
+    switch (num){
+        case 1:
+        cout<<"one";
+        break;
+
+        case 2:
+        cout<<"two";
+        break;
+        
+        default:
+        cout<<"other";
+        break;
+    }
+
+    return 0;
+}
